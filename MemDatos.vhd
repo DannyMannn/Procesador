e41a77842b7eea33c8486ae08aeca9850557f549
Danny <65949272+DannyMannn@users.noreply.github.com>
2023-10-24 13:13:39 -0600

    Agrega MemDatos - David

--- MemDatos.vhd
+++ MemDatos.vhd
@@ -1,41 +1,30 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date:    16:31:59 10/18/2023 
--- Design Name: 
--- Module Name:    MemDatos - Behavioral 
--- Project Name: 
--- Target Devices: 
--- Tool versions: 
--- Description: 
---
--- Dependencies: 
---
--- Revision: 
--- Revision 0.01 - File Created
--- Additional Comments: 
---
-----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx primitives in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 entity MemDatos is
+Port(
+        clk, mem_write, mem_read : in STD_LOGIC;
+        addr : in STD_LOGIC_VECTOR(5 downto 0);
+        dwrite : in STD_LOGIC_VECTOR(15 downto 0);
+        dread : out STD_LOGIC_VECTOR(15 downto 0)
+    );
 end MemDatos;
 
 architecture Behavioral of MemDatos is
-
+type ram_type is array(31 downto 0) of STD_LOGIC_VECTOR(15 downto 0);
+    signal RAM : ram_type;
 begin
-
-
+    process(clk, mem_write)
+    begin
+        if clk'event and clk='1' then
+            if mem_write='1' then
+                RAM(conv_integer(addr)) <= dwrite;
+                dread <= dwrite;
+            end if;
+            if mem_read='1' then
+                dread <= RAM(conv_integer(addr));
+            end if;
+        end if;
+    end process;
 end Behavioral;
-
